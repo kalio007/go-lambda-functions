82a583cd623d9c39f03ba4ef6bd0e8b0e9d7db34
kalio007 <kalioprince1@gmail.com>
2024-09-12 21:09:56 +0100

    basic event handler

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "github.com/aws/aws-lambda-go/lambda"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
-func handle 
+
+type MyEvent struct {
+	Name string `json:"name?"`
+	Age  int    `json:"age?"`
+}
+type MyResponse struct {
+	Message string `json:"answer"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil
+}
+
 func main() {
-	
+	lambda.Start(HandleLambdaEvent)
 }
